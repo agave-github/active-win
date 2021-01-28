5b3c5440ff3d820fa6c744672fef00311e170e6b
achuinard <tony@twansoftware.com>
2021-01-28 16:42:34 -0500

    rm sys

--- prepare.js
+++ prepare.js
@@ -1,8 +1,7 @@
-var sys = require('sys');
 var exec = require('child_process').exec;
 var os = require('os');
 
-function puts(error, stdout, stderr) { sys.puts(stdout) }
+function puts(error, stdout, stderr) { console.log(stdout) }
 
 if (os.type() === 'Darwin')
 	exec("npm run build", puts);
